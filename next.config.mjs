9187e2d47b85eeba193d94614ed0ca9ddc357242
Youssef Barj <youssef@edtechiecorp.com>
2026-05-12 18:27:14 +0100

    Migrate to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
-
 export default nextConfig
